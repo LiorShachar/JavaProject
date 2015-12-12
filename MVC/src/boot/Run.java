0a50a7dc12c3b6c70ee299b3166a7f84b0bb1a90
LiorShachar <liorzebay@gmail.com>
2015-12-13 00:26:40 +0200

    12:26

--- MVC/src/boot/Run.java
+++ MVC/src/boot/Run.java
@@ -1,9 +1,22 @@
 package boot;
 
+import controller.Controller;
+import controller.MyController;
+import model.Model;
+import model.MyModel;
+import view.MyView;
+import view.View;
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Controller c = new MyController();
+		   View v = new MyView(c);
+		   Model m = new MyModel(c);
+		   c.setModel(m);
+		   c.setView(v);
+		   v.start();
+
 
 	}
 
