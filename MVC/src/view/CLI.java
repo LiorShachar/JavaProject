86c41aebd4919037afde08b89b89c9a4255eb1fb
LiorShachar <liorzebay@gmail.com>
2015-12-12 23:54:02 +0200

    23:53

--- MVC/src/view/CLI.java
+++ MVC/src/view/CLI.java
@@ -1,5 +1,34 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import controller.Command;
+
 public class CLI {
 
+	private HashMap<String, Command> hmap;
+	private BufferedReader in;
+	private PrintWriter out;
+	
+
+	
+	
+	 public CLI(HashMap<String, Command> hmap, BufferedReader in, PrintWriter out) {
+		super();
+		this.hmap = hmap;
+		this.in = in;
+		this.out = out;
+	}
+
+
+
+
+	//THREADED
+	public void start(){
+		//TODO
+	}
+	
+	
 }
