10e627efae4498a6564effebef8a35a521346931
LiorShachar <liorzebay@gmail.com>
2015-12-12 22:39:42 +0200

    22:39

---
+++ MVC/CLI.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.io.PrintWriter;
+
+public class CLI {
+
+	private HashMap<String,Command>;
+	private BufferedReader in;
+	private PrintWriter out;
+	
+	public void start(){
+		//TODO
+	}
+	
+	
+}
